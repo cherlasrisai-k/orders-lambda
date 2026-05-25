678c8aefa4e8f646211940ff33f8d28fef3d2445
CherlaSrisai.K <cherlasrisai.k@encora.com>
2026-05-25 17:08:12 +0530

    Added a log

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ let {
                         }
                     })
                 );
-
+            console.log("Get Request");
             return response(
                 200,
                 {
